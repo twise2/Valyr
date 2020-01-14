d2283681fa6af6d91355d13a6552c0100ad3cf3a
Tristan Wise <tristan@tristanwise.com>
2020-01-13 21:35:31 -0500

    clean

--- code/lint_units.js
+++ code/lint_units.js
@@ -2,13 +2,14 @@
 const path = require('path');
 const fs = require('fs');
 //joining path of directory
-const directoryPath = path.join("./../units/");
+const directoryPath = path.join(__dirname+"./../units/");
 
 fs.readdir(directoryPath, function (err, files) {
     //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     }
+    console.log("Printing All Parsing Errors...")
     //listing all files using forEach
     files.forEach(function (file) {
         fs.readFile(directoryPath+file, 'utf8', function(err, contents) {
@@ -22,4 +23,5 @@ fs.readdir(directoryPath, function (err, files) {
             }
         });
     });
+    console.log("Done.")
 });
