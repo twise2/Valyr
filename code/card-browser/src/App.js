c267ffc90d90145c4fbf994658f49892060cad51
Tristan Wise <tristan@tristanwise.com>
2020-01-21 10:11:06 -0600

    printable

--- code/card-browser/src/App.js
+++ code/card-browser/src/App.js
@@ -8,7 +8,7 @@ import energy_icon from './icons/energy_icon.svg';
 import move_icon from './icons/move_icon.svg';
 import range_icon from './icons/range_icon.svg';
 import appraisal_icon from './icons/appraisal_icon.svg';
-import chevron_up from './icons/up-chevron.svg';
+//import chevron_up from './icons/up-chevron.svg';
 
 class App extends Component {
   constructor(props) {
@@ -59,14 +59,14 @@ class App extends Component {
     });
   }
 
-  RangeStats() {
-    let ranges = this.state.data[this.state.currentIndex].Combat.Range.split(
-      /\(|\)/,
-    );
+  RangeStats(index) {
+    let ranges = this.state.data[index].Combat.Range.split(/\(|\)/);
     return (
       <div
         className="Stat"
-        style={{height: '1this.state.currentIndexcurrentIndex%', alignItems: 'flex-end'}}>
+        style={{
+          alignItems: 'flex-end',
+        }}>
         <div className="RangeStats">{this.RangeModifier(ranges)}</div>
         <img src={range_icon} className="StatIcon" />
       </div>
@@ -75,102 +75,104 @@ class App extends Component {
 
   render() {
     if (!this.state.data) return null;
-    let Identity = this.state.data[this.state.currentIndex].Identity;
-    console.log(Identity);
     return (
       <div className="App">
-        <div className="HorizontalFlex">
-          {this.state.mouseMoving ? (
-            <img
-              src={chevron_up}
-              resizeMode="contain"
-              className="StatIcon"
-              style={{transform: 'rotate(270deg)', flex: 1}}
-              onClick={() => this.lastCard()}
-            />
-          ) : null}
-          <div className="Card">
-            <header className="CardHeader">
-              <text
-                numberOfLines={1}
-                adjustsFontSizeToFit={true}
-                className="CardName">
-                {this.state.data[this.state.currentIndex].Identity.Name}
-              </text>
-              <div className="HealthContainer">
-                {this.state.data[this.state.currentIndex].Combat.Health}
-              </div>
-            </header>
-            <header className="ContentBody">
-              <header className="Content">
-                <div className="CharacterIdentity">
-                  {Identity.Size}-{Identity.Type}
-                  {'\n'}
-                  {Identity.Dynasty} - {Identity.Species} - {Identity.Class}
-                </div>
-                <div className="Abilities">
-                  {this.state.data[this.state.currentIndex].Abilities.map(
-                    ability => {
-                      return (
-                        <div>
-                          <div className="AbilityHeader">
-                            {ability.Name} - {ability.Type}
+        {this.state.data.map((card, index) => {
+          let Identity = this.state.data[index].Identity;
+          return (
+            <div className="HorizontalFlex">
+              {/*
+              {this.state.mouseMoving ? (
+                <img
+                  src={chevron_up}
+                  resizeMode="contain"
+                  className="StatIcon"
+                  style={{transform: 'rotate(270deg)', flex: 1}}
+                  onClick={() => this.lastCard()}
+                />
+              ) : null}
+          */}
+              <div className="Card">
+                <header className="CardHeader">
+                  <text
+                    numberOfLines={1}
+                    adjustsFontSizeToFit={true}
+                    className="CardName">
+                    {this.state.data[index].Identity.Name}
+                  </text>
+                  <div className="HealthContainer">
+                    {this.state.data[index].Combat.Health}
+                  </div>
+                </header>
+                <header className="ContentBody">
+                  <header className="Content">
+                    <div className="CharacterIdentity">
+                      {Identity.Size}-{Identity.Type}
+                      {'\n'}
+                      {Identity.Dynasty} - {Identity.Species} - {Identity.Class}
+                    </div>
+                    <div className="Abilities">
+                      {this.state.data[index].Abilities.map(ability => {
+                        return (
+                          <div>
+                            <div className="AbilityHeader">
+                              {ability.Name} - {ability.Type}
+                            </div>
+                            <div className="AbilityText">{ability.Ability}</div>
                           </div>
-                          <div className="AbilityText">{ability.Ability}</div>
-                        </div>
-                      );
-                    },
-                  )}
-                </div>
-              </header>
-              <header className="Stats">
-                {this.RangeStats()}
-                <div className="Stat">
-                  <div className="StatNumber">
-                    {this.state.data[this.state.currentIndex].Combat.Attack}
-                  </div>
-                  <img src={attack_icon} className="StatIcon" />
-                </div>
-                <div className="Stat">
-                  <div className="StatNumber">
-                    {this.state.data[this.state.currentIndex].Combat.Defense}
-                  </div>
-                  <img src={defense_icon} className="StatIcon" />
-                </div>
-                <div className="Stat">
-                  <div className="StatNumber">
-                    {this.state.data[this.state.currentIndex].Combat.Energy}
-                  </div>
-                  <img src={energy_icon} className="StatIcon" />
-                </div>
-                <div className="Stat">
-                  <div className="StatNumber">
-                    {this.state.data[this.state.currentIndex].Combat.Movement}
-                  </div>
-                  <img src={move_icon} className="StatIcon" />
-                </div>
-                <div className="Stat">
-                  <div className="StatNumber">
-                    {this.state.data[this.state.currentIndex].Combat.Appraisal}
-                  </div>
-                  <img
-                    src={appraisal_icon}
-                    className="StatIcon"
-                  />
-                </div>
-              </header>
-            </header>
-          </div>
-          {this.state.mouseMoving ? (
-            <img
-              src={chevron_up}
-              resizeMode="contain"
-              className="StatIcon"
-              style={{transform: 'rotate(90deg)', flex: 1}}
-              onClick={() => this.nextCard()}
-            />
-          ) : null}
-        </div>
+                        );
+                      })}
+                    </div>
+                  </header>
+                  <header className="Stats">
+                    {this.RangeStats(index)}
+                    <div className="Stat">
+                      <div className="StatNumber">
+                        {this.state.data[index].Combat.Attack}
+                      </div>
+                      <img src={attack_icon} className="StatIcon" />
+                    </div>
+                    <div className="Stat">
+                      <div className="StatNumber">
+                        {this.state.data[index].Combat.Defense}
+                      </div>
+                      <img src={defense_icon} className="StatIcon" />
+                    </div>
+                    <div className="Stat">
+                      <div className="StatNumber">
+                        {this.state.data[index].Combat.Energy}
+                      </div>
+                      <img src={energy_icon} className="StatIcon" />
+                    </div>
+                    <div className="Stat">
+                      <div className="StatNumber">
+                        {this.state.data[index].Combat.Movement}
+                      </div>
+                      <img src={move_icon} className="StatIcon" />
+                    </div>
+                    <div className="Stat">
+                      <div className="StatNumber">
+                        {this.state.data[index].Combat.Appraisal}
+                      </div>
+                      <img src={appraisal_icon} className="StatIcon" />
+                    </div>
+                  </header>
+                </header>
+              </div>
+              {/*
+                {this.state.mouseMoving ? (
+                <img
+                  src={chevron_up}
+                  resizeMode="contain"
+                  className="StatIcon"
+                  style={{transform: 'rotate(90deg)', flex: 1}}
+                  onClick={() => this.nextCard()}
+                />
+              ) : null}
+              */}
+            </div>
+          );
+        })}
       </div>
     );
   }
